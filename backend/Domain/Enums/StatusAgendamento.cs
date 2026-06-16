56ceaf12aab0b5757f3321c554a32be74bcb0c05
ThauannydaCruzz <thauannydacruzz@gmail.com>
2026-06-16 18:42:36 -0300

    removendo página não mais utilizada

--- backend/Domain/Enums/StatusAgendamento.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Enums;
-
-public enum StatusAgendamento
-{
-    Pendente = 1,
-    Confirmado = 2,
-    Concluido = 3,
-    Cancelado = 4
-}
--- frontend/src/utils/situacoes.ts
+++ frontend/src/utils/situacoes.ts
@@ -1,2 +1,2 @@
 export const textoSituacao: any = { 1: 'Pendente', 2: 'Confirmado', 3: 'Concluído', 4: 'Cancelado' }
-export const corSituacao: any = { 1: 'situacao pendente', 2: 'situacao confirmado', 3: 'situacao confirmado', 4: 'situacao cancelado' }
+export const corSituacao: any = { 1: 'situacao pendente', 2: 'situacao confirmado', 3: 'situacao concluido', 4: 'situacao cancelado' }
